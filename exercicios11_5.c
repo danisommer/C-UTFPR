c147d414ad79e28f3bcfea57f7c658aa072b5181
danisommer <danibee2005@gmail.com>
2023-04-27 22:35:00 -0300

    exercicios

--- exercicios11_5.c
+++ exercicios11_5.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#define TAMANHO_GRAFICO 10
+#define TAMANHO_GRAFICO 24
 
 int j=1;
 
 int leProximoDado (int x){
 
     if (j>TAMANHO_GRAFICO){
-        return x;
+        return 0;
 
     }
-    printf("\ninsira o valor da posicao %d: ", j);
+    printf("\ninsira a quantidade de pessoas as %.2dh00: ", j);
     scanf("%d", &x);
     j++;
     return x;
@@ -20,19 +20,19 @@ int leProximoDado (int x){
 void registraPadrao (int posicao, int tipo){
 
 if(tipo == 1){
-    printf("\npico na posicao %d\n", posicao);
+    printf("\npico no horario %.2dh00\n", posicao);
 
 }
 if(tipo == 2){
-    printf("\ndescida na posicao %d\n", posicao);
+    printf("\ndescida no horario %.2dh00\n", posicao);
 
 }
 if(tipo == 3){
-    printf("\nvale na posicao %d\n", posicao);
+    printf("\nvale no horario %.2dh00\n", posicao);
 
 }
 if(tipo == 4){
-    printf("\nsubida na posicao %d\n", posicao);
+    printf("\nsubida no horario %.2dh00\n", posicao);
 
 }
 }
@@ -45,27 +45,27 @@ int n1, n2, n3, i;
     n2 =  leProximoDado(0);
     n3 =  leProximoDado(0);
 
-    for (i=0;i<=TAMANHO_GRAFICO-3;i++){
+    for (i=2;i<=TAMANHO_GRAFICO-1;i++){
 
         if (n2>n1 && n2>n3){
 
-            registraPadrao(i+2,1);
+            registraPadrao(i,1);
 
         }
         if (n2<n1 && n2>n3){
 
-            registraPadrao(i+2,2);
+            registraPadrao(i,2);
 
         }
 
         if (n2<n1 && n2<n3){
 
-            registraPadrao(i+2,3);
+            registraPadrao(i,3);
 
         }
         if (n2>n1 && n2<n3){
 
-            registraPadrao(i+2,4);
+            registraPadrao(i,4);
 
         }
         n1=n2;
